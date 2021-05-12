7276e5fed4e5034d21357d7f644aed11e7d02216
saidi zied <saidiziedg@gmail.com>
2021-05-12 21:39:57 +0100

    create home views and controller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WishList.Controllers {
+
+
+    public class HomeController : Controller {
+
+
+
+public IActionResult Index(){
+    return View("Index");
+}
+
+public IActionResult Error(){
+    return View("Error");
+}
+
+    }
+}
