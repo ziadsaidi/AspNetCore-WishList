9cf76e10c5e78f35a6820e01bfae6a8cdbb7b162
saidi zied <saidiziedg@gmail.com>
2021-05-12 21:53:22 +0100

    fix ef configuration issues

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,10 +18,8 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>{
-                options.UseInMemoryDatabase("WishList");
-            });
-            
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
+
             services.AddMvc();
         }
 
