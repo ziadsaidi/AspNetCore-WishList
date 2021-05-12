05fc6f2aa1169e78cbf3414ecf00172fc3849bcf
saidi zied <saidiziedg@gmail.com>
2021-05-12 21:24:50 +0100

    add middleware config

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,15 +17,29 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            //check if the env is Dev Or Prod 
+           if( env.IsDevelopment()){
+               app.UseDeveloperExceptionPage();
+           }
+
+           else {
+               app.UseExceptionHandler("/Home/Error");
+           }
+
+           app.UseRouting();
+
+           app.UseEndpoints(endpoints =>{
+               endpoints.MapDefaultControllerRoute();
+
+           });
         }
     }
 }
